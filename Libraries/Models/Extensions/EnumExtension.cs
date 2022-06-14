a0e98ab637432bcec8d9d16c807861f9557ae0cd
Roder <roder@begonia-design.com.tw>
2022-06-14 16:57:28 +0800

    Modify

--- Libraries/Models/Extensions/EnumExtension.cs
+++ Libraries/Models/Extensions/EnumExtension.cs
@@ -10,22 +10,24 @@ namespace Models.Extensions
         /// <summary>
         /// Get Enum Name
         /// </summary>
-        /// <param name="value">Enum</param>
+        /// <param name="enumValue">Enum</param>
         /// <param name="type">Attribute type</param>
         /// <returns>Enum Name</returns>
-        public static string GetAttributeContent(this Enum value, AttributeType type = AttributeType.Display)
+        public static string GetAttributeContent(this Enum enumValue, AttributeType type = AttributeType.Display)
         {
             if (type == AttributeType.Description)
             {
-                var descriptionAttribute = GetCustomAttribute<DescriptionAttribute>(value);
-                return string.IsNullOrWhiteSpace(descriptionAttribute?.Description) ? value.ToString() : descriptionAttribute.Description;
+                var descriptionAttribute = getCustomAttribute<DescriptionAttribute>(enumValue);
+                return getValueOrDefault(descriptionAttribute?.Description);
             }
 
-            var displayAttribute = GetCustomAttribute<DisplayAttribute>(value);
-            return string.IsNullOrWhiteSpace(displayAttribute?.Name) ? value.ToString() : displayAttribute.Name;
+            var displayAttribute = getCustomAttribute<DisplayAttribute>(enumValue);
+            return getValueOrDefault(displayAttribute?.Name);
 
-            TAttribute? GetCustomAttribute<TAttribute>(Enum value) where TAttribute : Attribute
+            TAttribute? getCustomAttribute<TAttribute>(Enum value) where TAttribute : Attribute
                 => value.GetType().GetField(value.ToString())?.GetCustomAttribute<TAttribute>(false);
+
+            string getValueOrDefault(string? value) => string.IsNullOrWhiteSpace(value) ? enumValue.ToString() : value;
         }
     }
 }
